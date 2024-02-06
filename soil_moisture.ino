bba80cc3b3e6a2d7a97cc88099354afd66c3c5c4
engskie09 <rafaelmasallo@gmail.com>
2024-02-07 04:27:32 +0800

    ✨ feat: 🎨 refactored soil_moisture and 🪝 handled wet by percentage

--- soil_moisture.ino
+++ soil_moisture.ino
@@ -1,71 +1,42 @@
-// Arduino soil moisture sensor code
+const int moistureSensorPin = A0;
+float moistureValue;
+float moisturePercentage;
 
-// declare variables for pins
-const int sensorpin = A0;
-const int sensorpower = 8;
-const int LED1 = 2;
-const int LED2 = 3;
-const int LED3 = 4;
+// thresholds
+float maxAnalogValue = 539.00;
+float minWetPercentage = 70.00;
 
-// variable for sensor reading
-int sensor;
-
-// delay time between sensor readings (milliseconds)
-// this helps prevent the sensor from corroding by
-// not leaving it powered on all the time
-// (note: the Tinkercad simulation can run slowly,
-// you may want to decrease the delay time if running
-// this in Tinkercad
-const int delayTime = 1000; 
-
-// "wet" and "dry" thresholds - these require calibration
-int wet = 800;
-int dry = 500;
-
-void setup(){ // code that only runs once
-  // set pins as outputs
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-  pinMode(sensorpower,OUTPUT);
-  
-  // initialize serial communication
+void setup(){
   Serial.begin(9600);
 }
 
-void loop(){ // code that loops forever
-  // power on sensor and wait briefly
-  digitalWrite(sensorpower,HIGH);
-  delay(10);
-  // take reading from sensor
-  sensor = analogRead(sensorpin);
-  // turn sensor off to help prevent corrosion
-  digitalWrite(sensorpower,LOW);
+void loop() {
+  delay(500);
+
+  moistureValue = analogRead(moistureSensorPin);
+  moisturePercentage = (moistureValue/maxAnalogValue)*100;
+
+  Serial.print(moisturePercentage);
+  Serial.println('%');
+
+  Serial.println("moisturePercentage");
+  Serial.println(moisturePercentage);
+
+  Serial.println("minWetPercentage");
+  Serial.println(minWetPercentage);;
   
-  // print sensor reading
-  Serial.println(sensor);
-  
-  // If sensor reading is greater than "wet" threshold,
-  // turn on the blue LED. If it is less than the "dry"
-  // threshold, turn on the red LED. If it is in between
-  // the two values, turn on the yellow LED.
-  if(sensor>wet){
-    digitalWrite(LED1,LOW);
-    digitalWrite(LED2,LOW);
-    digitalWrite(LED3,HIGH);
+  Serial.println("-----------------------------------");
+  Serial.println("-----------------------------------");
+  Serial.println("-----------------------------------");
+  Serial.println("-----------------------------------");
+  Serial.println("-----------------------------------");
+
+  if (moisturePercentage>minWetPercentage) {
+    Serial.println("Wet");
   }
-  else if(sensor<dry){
-    digitalWrite(LED1,HIGH);
-    digitalWrite(LED2,LOW);
-    digitalWrite(LED3,LOW);
+  else {
+    Serial.println("Dry");
   }
-  else{
-    digitalWrite(LED1,LOW);
-    digitalWrite(LED2,HIGH);
-    digitalWrite(LED3,LOW);
-  }
-  
-  // wait before taking next reading
-  delay(delayTime);
-  
+
+  delay(500);
 }
