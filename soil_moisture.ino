578d029363fcad25c84712dd82c32ac53fc3187d
engskie09 <rafaelmasallo@gmail.com>
2024-02-07 05:18:38 +0800

    ✨ feat: 💦 finalized detecting of humidity

--- soil_moisture.ino
+++ soil_moisture.ino
@@ -4,39 +4,45 @@ float moisturePercentage;
 
 // thresholds
 float maxAnalogValue = 539.00;
-float minWetPercentage = 70.00;
+
+char* const getMoisture() {
+  moistureValue = analogRead(moistureSensorPin);
+  moisturePercentage = (moistureValue / maxAnalogValue)*100;
+
+  Serial.print(moisturePercentage);
+  Serial.println('%');
+
+  Serial.println("moistureValuev");
+  Serial.println(moistureValue);
+  
+  Serial.println("moisturePercentage");
+  Serial.println(moisturePercentage);
+  
+  Serial.println("-----------------------------------");
+  Serial.println("-----------------------------------");
+  Serial.println("--------------DIVIDER--------------");
+  Serial.println("--------------DIVIDER--------------");
+  Serial.println("--------------DIVIDER--------------");
+  Serial.println("-----------------------------------");
+  Serial.println("-----------------------------------");
+
+  if (moisturePercentage > 70.00) {
+    return ("WET");
+  } else if (moisturePercentage < 30.00) {
+    return ("DRY");
+  } else {
+    return ("MILD");
+  }
+}
 
 void setup(){
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(500);
+  char* const humidity = getMoisture();
 
-  moistureValue = analogRead(moistureSensorPin);
-  moisturePercentage = (moistureValue/maxAnalogValue)*100;
+  Serial.println(humidity);
 
-  Serial.print(moisturePercentage);
-  Serial.println('%');
-
-  Serial.println("moisturePercentage");
-  Serial.println(moisturePercentage);
-
-  Serial.println("minWetPercentage");
-  Serial.println(minWetPercentage);;
-  
-  Serial.println("-----------------------------------");
-  Serial.println("-----------------------------------");
-  Serial.println("-----------------------------------");
-  Serial.println("-----------------------------------");
-  Serial.println("-----------------------------------");
-
-  if (moisturePercentage>minWetPercentage) {
-    Serial.println("Wet");
-  }
-  else {
-    Serial.println("Dry");
-  }
-
-  delay(500);
+  delay(1000);
 }
